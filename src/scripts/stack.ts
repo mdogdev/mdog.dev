f419c233865f48ece8ac5869f9a371806d64836c
Mdog504 <mdog@mdog.xyz>
2024-09-18 20:51:03 +0100

    404, about, homepage

---
+++ src/scripts/stack.ts
@@ -0,0 +1,60 @@
+export const stack = {
+    Frontend: [
+        {
+            name: "TypeScript",
+            href: "https://www.typescriptlang.org/",
+        },
+        {
+            name: "React",
+            href: "https://reactjs.org/",
+        },
+        {
+            name: "Astro",
+            href: "https://astro.build/",
+        },
+    ],
+    Backend: [
+        {
+            name: "Nodejs",
+            href: "https://nodejs.org/",
+        },
+        {
+            name: "Python",
+            href: "https://www.python.org/",
+        },
+        {
+            name: "Java",
+            href: "https://www.java.com/",
+        },
+    ],
+    Database: [
+        {
+            name: "MongoDB",
+            href: "https://www.mongodb.com/",
+        },
+        {
+            name: "MySQL",
+            href: "https://www.mysql.com/",
+        },
+
+    ],
+    Linux: [
+        {
+            name: "Debian",
+            href: "https://www.debian.org/",
+        },
+        {
+            name: "ArchLinux",
+            href: "https://www.archlinux.org/",
+        },
+        {
+            name: "Docker",
+            href: "https://www.docker.com/",
+        },
+        {
+            name: "Kubernetes",
+            href: "https://kubernetes.io/",
+        },
+    ],
+    
+}
